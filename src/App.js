13fd9841acbe9cf1256b22c7c0f6a8022a26bcf6
Anu-tochka <anuta-zh@mail.ru>
2021-07-20 19:30:52 +0300

    Added State

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import Message from './Message.js';
+//import Message from './Message.js';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 	let mes = "Hello!";
   return (
-   
-   <Message message={mes} />
+    <div id='msg'></div>
  
   );
 }
 
 export default App;
+
+function FragmentMsg() {
+ return (
+   <>
+    
+     <input type="text" value={author}  id='authorN'/>
+     <input type="text" value={text}  id='textN' />
+     <button className="write" onClick={sendMsg}>Click!</button>
+   </>
+ );
+}
+
+  ReactDOM.render(<FragmentMsg />, document.getElementById('msg'));
+  
+export function messageList() {
+ const [msg, setMsg] = useState([]);
+ 
+ return msg.map((author, text) => <div>
+     <span className="author">{setMsg(author)}</span>
+   <div>
+     {setMsg(text)}
+   </div>
+   </div>);
+ 
+function sendMsg() {
+   let authorN = document.getElementById('authorN');
+   let textN = document.getElementById('textN');
+   authorN.push({textN, authorN});
+ }
+ 
+ useEffect(() => {
+   '<div>' + author + ', hello!<div>'
+ }, [author]);
+
+}
