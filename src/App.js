645ffa00f8d441eeef82d053e01ecad4f4ba0b3a
Anu-tochka <anuta-zh@mail.ru>
2021-07-23 19:32:08 +0300

    Added MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,48 +1,59 @@
-//import Message from './Message.js';
+import Message from './Message.js';
 import './App.css';
 import { useState } from 'react';
+import { TextField } from "@material-ui/core";
+import Button from '@material-ui/core/Button';
 
 function App() {
-	let mes = "Hello!";
-  return (
-    <div id='msg'></div>
+ const [messageList, setmessageList] = React.useState(initialState: [])
+ const [inputValue, setInputValue] = React.useState(initialState: '')
  
+ React.useEffect(effect() => {
+   if (messageList[messageList.length-1].author != 'bot'){
+	   setmessageList{author: 'bot', text:'hello!'
+ }, deps: [messageList]);
+
+ const handleMessageChange = (e) => {
+	 setInputValue(e.target.Value)
+ }
+ 
+  return (
+    <div className='app'>
+	<List subheader={<li />} className={classes.list}>
+  {[name, id].map((sectionId) => (
+    <li key={`section-${sectionId}`} className={classes.listSection}>
+      <ListItem key={`item-${sectionId}-${item}`}>
+            <ListItemText primary={`Item ${item}`} />
+          </ListItem>
+    </li>
+  ))}
+</List>
+
+<div
+	{messageList.map((message, index:number) => (
+     <Message key={index} author={message.author} text={message.text} />
+     ))}
+	 <TextField autofocus
+     style={{ margin: '20px' }}
+     id="inputValue"
+     label="Outlined"
+     variant="outlined"
+     value={inputValue}
+     onChange={handleMessageChange}
+   />
+	 <TextField
+     style={{ margin: '20px' }}
+     id="author"
+     label="Outlined"
+     variant="outlined"
+     value={author}
+   />
+	 <Button variant="contained" color="secondary onClick={sendMsg}>Click!</Button>
+   </div>
+  </div>
   );
 }
 
 export default App;
-
-function FragmentMsg() {
- return (
-   <>
-    
-     <input type="text" value={author}  id='authorN'/>
-     <input type="text" value={text}  id='textN' />
-     <button className="write" onClick={sendMsg}>Click!</button>
-   </>
- );
-}
-
-  ReactDOM.render(<FragmentMsg />, document.getElementById('msg'));
-  
-export function messageList() {
- const [msg, setMsg] = useState([]);
- 
- return msg.map((author, text) => <div>
-     <span className="author">{setMsg(author)}</span>
-   <div>
-     {setMsg(text)}
-   </div>
-   </div>);
- 
-function sendMsg() {
-   let authorN = document.getElementById('authorN');
-   let textN = document.getElementById('textN');
-   authorN.push({textN, authorN});
- }
- 
- useEffect(() => {
-   '<div>' + author + ', hello!<div>'
- }, [author]);
 
 }
--- src/Message.js
+++ src/Message.js
@@ -3,7 +3,7 @@ import './Message.css';
 function Message(props) {
   return (
     <div className="Message" style={{marginTop: '50px'}}>
-      {props.message}
+      {props.author}: {props.text}
     </div>
   );
 }
